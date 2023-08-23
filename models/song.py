b2d93e39619ac23ed360478a1030a01d3dcab6da
Lydia Kuehnert <lydiakuehnert@gmail.com>
2023-08-23 18:47:25 -0400

    working on models

---
+++ models/song.py
@@ -0,0 +1,30 @@
+from .db import db
+from .like import likes
+
+
+class Song(db.Model):
+    __tablename__ = 'songs'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(255), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    image = db.Column(db.String(250), nullable=False)
+    audio = db.Column(db.String(255), nullable=False)
+
+    # relationship attributes
+    user = db.relationship("User", back_populates="songs")
+    song_likes = db.relationship(
+        "User",
+        secondary=likes,
+        back_populates="user_likes"
+    )
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "image": self.image,
+            "audio": self.audio,
+            "user": self.user.to_dict_no_song(),
+            "likes": len(self.song_likes)
+       }
---
+++ models/user.py
@@ -0,0 +1,41 @@
+from .db import db
+from .like import likes
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    firstname = db.Column(db.String(100))
+    lastname = db.Column(db.String(100))
+    password = db.Column(db.String(100), nullable=False)
+
+    # relationship attributes
+    songs = db.relationship("Song", back_populates="user", cascade="all, delete-orphan")
+    user_likes = db.relationship(
+        "Song",
+        secondary=likes,
+        back_populates="song_likes"
+    )
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "email": self.email,
+            "songs": [song.to_dict() for song in self.songs],
+            "likes": len(self.user_likes)
+        }
+    
+    def to_dict_no_song(self):
+        return {
+            "id": self.id,
+            "username": self.username,
+            "firstname": self.firstname,
+            "lastname": self.lastname,
+            "email": self.email
+        }
