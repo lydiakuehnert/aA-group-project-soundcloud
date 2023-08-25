252ba4a65369723c65ea6ec627fa29e5c2212e81
fishcakefish <fishcakeow@gmail.com>
2023-08-25 15:07:37 -0700

    fixed song seeders

--- app/seeds/songs.py
+++ app/seeds/songs.py
@@ -27,7 +27,7 @@ def seed_songs(all_users):
         )
     song4 = Song(
         name='Song4',
-        user_id=4,
+        user_id=1,
         image='https://icon2.cleanpng.com/20171220/bfe/number-4-png-5a3a51be177033.0271884715137714540962163.jpg',
         audio='dummydata.mp4',
         song_likes=sample(all_users, randint(0, len(all_users)))
@@ -35,7 +35,7 @@ def seed_songs(all_users):
 
     song5 = Song(
         name='Song5',
-        user_id=5,
+        user_id=2,
         image='https://e7.pngegg.com/pngimages/916/551/png-clipart-number-drawing-number-5-text-logo.png',
         audio='dummydata.mp4',
         song_likes=sample(all_users, randint(0, len(all_users)))
@@ -43,7 +43,7 @@ def seed_songs(all_users):
 
     song6 = Song(
         name='Song6',
-        user_id=6,
+        user_id=3,
         image='https://png.pngtree.com/png-clipart/20210123/ourmid/pngtree-black-number-6-png-image_2787060.jpg',
         audio='dummydata.mp4',
         song_likes=sample(all_users, randint(0, len(all_users)))
@@ -51,7 +51,7 @@ def seed_songs(all_users):
 
     song7 = Song(
         name='Song7',
-        user_id=7,
+        user_id=1,
         image='https://png.pngtree.com/png-clipart/20210328/ourmid/pngtree-pattern-number-7-glowing-color-png-image_3128574.jpg',
         audio='dummydata.mp4',
         song_likes=sample(all_users, randint(0, len(all_users)))
@@ -59,7 +59,7 @@ def seed_songs(all_users):
 
     song8 = Song(
         name='Song8',
-        user_id=8,
+        user_id=2,
         image='https://w7.pngwing.com/pngs/779/719/png-transparent-iphone-8-indore-police-child-minecraft-number-8-english-text-trademark-thumbnail.png',
         audio='dummydata.mp4',
         song_likes=sample(all_users, randint(0, len(all_users)))
@@ -67,7 +67,7 @@ def seed_songs(all_users):
 
     song9 = Song(
         name='Song9',
-        user_id=9,
+        user_id=3,
         image='https://pngimg.com/uploads/number9/number9_PNG19129.png',
         audio='dummydata.mp4',
         song_likes=sample(all_users, randint(0, len(all_users)))
@@ -75,7 +75,7 @@ def seed_songs(all_users):
 
     song10 = Song(
         name='Song10',
-        user_id=10,
+        user_id=1,
         image='https://w7.pngwing.com/pngs/622/298/png-transparent-simple-number-10-miscellaneous-text-trademark-thumbnail.png',
         audio='dummydata.mp4',
         song_likes=sample(all_users, randint(0, len(all_users)))
@@ -83,7 +83,7 @@ def seed_songs(all_users):
 
     song11 = Song(
         name='Song11',
-        user_id=11,
+        user_id=2,
         image='https://png.pngtree.com/png-vector/20210208/ourmid/pngtree-3d-numbers-11-in-a-circle-on-transparent-background-png-image_2905099.jpg',
         audio='dummydata.mp4',
         song_likes=sample(all_users, randint(0, len(all_users)))
@@ -91,7 +91,7 @@ def seed_songs(all_users):
 
     song12 = Song(
         name='Song12',
-        user_id=12,
+        user_id=3,
         image='https://www.pngfind.com/pngs/m/15-150614_number-12-clipart-blue-rounded-rectangle-with-number.png',
         audio='dummydata.mp4',
         song_likes=sample(all_users, randint(0, len(all_users)))
@@ -99,7 +99,7 @@ def seed_songs(all_users):
 
     song13 = Song(
         name='Song13',
-        user_id=13,
+        user_id=1,
         image='https://w7.pngwing.com/pngs/965/612/png-transparent-youtube-13-miscellaneous-text-trademark-thumbnail.png',
         audio='dummydata.mp4',
         song_likes=sample(all_users, randint(0, len(all_users)))
@@ -107,7 +107,7 @@ def seed_songs(all_users):
 
     song14 = Song(
         name='Song14',
-        user_id=14,
+        user_id=2,
         image='https://png.pngtree.com/png-clipart/20210301/ourmid/pngtree-number-14-3d-rendering-png-image_2992758.jpg',
         audio='dummydata.mp4',
         song_likes=sample(all_users, randint(0, len(all_users)))
@@ -115,7 +115,7 @@ def seed_songs(all_users):
 
     song15 = Song(
         name='Song15',
-        user_id=15,
+        user_id=3,
         image='https://png.pngtree.com/png-clipart/20210301/ourmid/pngtree-number-15-3d-rendering-png-image_2992759.jpg',
         audio='dummydata.mp4',
         song_likes=sample(all_users, randint(0, len(all_users)))
@@ -123,7 +123,7 @@ def seed_songs(all_users):
 
     song16 = Song(
         name='Song16',
-        user_id=16,
+        user_id=1,
         image='https://png.pngtree.com/png-vector/20210216/ourmid/pngtree-black-gradient-3d-number-16-png-image_2923782.jpg',
         audio='dummydata.mp4',
         song_likes=sample(all_users, randint(0, len(all_users)))
@@ -131,7 +131,7 @@ def seed_songs(all_users):
 
     song17 = Song(
         name='Song17',
-        user_id=17,
+        user_id=2,
         image='https://png.pngtree.com/png-clipart/20210301/ourmid/pngtree-number-17-3d-rendering-png-image_2992761.jpg',
         audio='dummydata.mp4',
         song_likes=sample(all_users, randint(0, len(all_users)))
@@ -139,7 +139,7 @@ def seed_songs(all_users):
 
     song18 = Song(
         name='Song18',
-        user_id=18,
+        user_id=3,
         image='https://png.pngtree.com/png-clipart/20210411/original/pngtree-number-18-png-image_6212867.jpg',
         audio='dummydata.mp4',
         song_likes=sample(all_users, randint(0, len(all_users)))
@@ -147,7 +147,7 @@ def seed_songs(all_users):
 
     song19 = Song(
         name='Song19',
-        user_id=19,
+        user_id=1,
         image='https://www.pngkit.com/png/full/160-1607668_number-19-png.png',
         audio='dummydata.mp4',
         song_likes=sample(all_users, randint(0, len(all_users)))
@@ -155,7 +155,7 @@ def seed_songs(all_users):
 
     song20 = Song(
         name='Song20',
-        user_id=20,
+        user_id=2,
         image='https://www.clipartmax.com/png/small/11-119352_heartbeat-20clipart-clipart-panda-free-number-20-clipart.png',
         audio='dummydata.mp4',
         song_likes=sample(all_users, randint(0, len(all_users)))
