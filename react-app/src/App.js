f457efa5e3a506017e195093a6511784e05039e6
Lydia Kuehnert <lydiakuehnert@gmail.com>
2023-08-25 18:19:55 -0400

    frontend stuff

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,6 +5,11 @@ import SignupFormPage from "./components/SignupFormPage";
 import LoginFormPage from "./components/LoginFormPage";
 import { authenticate } from "./store/session";
 import Navigation from "./components/Navigation";
+import AllSongs from "./components/AllSongs";
+import OneSong from "./components/OneSong";
+import CreateSongForm from "./components/CreateSongForm";
+import EditSongForm from "./components/EditSongForm";
+import ManageSongs from "./components/ManageSongs";
 
 function App() {
   const dispatch = useDispatch();
@@ -23,6 +28,21 @@ function App() {
           </Route>
           <Route path="/signup">
             <SignupFormPage />
+          </Route>
+          <Route exact path="/">
+            <AllSongs />
+          </Route>
+          <Route exact path="/songs/new">
+            <CreateSongForm />
+          </Route>
+          <Route exact path="/songs/user">
+            <ManageSongs />
+          </Route>
+          <Route exact path="/songs/:songId">
+            <OneSong />
+          </Route>
+          <Route exact path="/songs/:songId/edit">
+            <EditSongForm />
           </Route>
         </Switch>
       )}
---
+++ react-app/src/store/songs.js
@@ -0,0 +1,186 @@
+const GET_SONGS = "songs/getSongs";
+const GET_SONG = "songs/getSong";
+const CREATE_SONG = "songs/createSong";
+const GET_USER_SONGS = "songs/getUserSongs";
+const DELETE_SONG = "songs/deleteSong";
+const EDIT_SONG = "songs/editSong";
+
+
+const getUserSongsAction = (songs) => {
+    return {
+        type: GET_USER_SONGS,
+        songs
+    }
+}
+
+const getSongAction = (song) => {
+    return {
+        type: GET_SONG,
+        song
+    }
+}
+
+const getSongsAction = (songs) => {
+    return {
+        type: GET_SONGS,
+        songs
+    }
+};
+
+const createSongAction = (song) => {
+    return {
+        type: CREATE_SONG,
+        song
+    }
+}
+
+const deleteSongAction = (songId) => {
+    return {
+        type: DELETE_SONG,
+        songId
+    }
+}
+
+const editSongAction = (song) => {
+    return {
+        type: EDIT_SONG,
+        song
+    }
+}
+
+export const getUserSongsThunk = () => async dispatch => {
+    const res = await fetch('/api/songs/current')
+
+    if (res.ok) {
+        const songs = await res.json();
+        dispatch(getUserSongsAction(songs.Songs))
+    }
+}
+
+export const getSongThunk = (songId) => async dispatch => {
+    const res = await fetch(`/api/songs/${songId}`);
+
+    if (res.ok) {
+        const song = await res.json()
+        dispatch(getSongAction(song))
+    }
+}
+
+export const getSongsThunk = () => async dispatch => {
+    const res = await fetch('/api/songs')
+
+    if (res.ok) {
+        const songs = await res.json();
+        dispatch(getSongsAction(songs.Songs))
+    }
+};
+
+export const createSongThunk = (payload) => async dispatch => {
+    try {
+        const { newSong, SongImages } = payload;
+        const res = await fetch('/api/songs', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(newSong)
+        })
+
+        if (res.ok) {
+            const song = await res.json();
+            for (let i = 0; i < SongImages.length; i++) {
+                let img = SongImages[i]
+                await fetch(`/api/songs/${song.id}/images`, {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(img)
+                })
+            }
+            dispatch(createSongAction(song))
+            return song;
+        }
+    } catch (e) {
+        const data = await e.json()
+        return data;
+    }
+}
+
+export const deleteSongThunk = (songId) => async dispatch => {
+    const res = await fetch(`/api/songs/${songId}`, {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' }
+    })
+    if (res.ok) {
+        dispatch(deleteSongAction(songId))
+    }
+}
+
+export const editSongThunk = (payload) => async dispatch => {
+    try {
+        const { newSong, SongImages } = payload;
+        const res = await fetch(`/api/songs/${newSong.id}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(newSong)
+        })
+
+        if (res.ok) {
+            const song = await res.json();
+            for (let i = 0; i < SongImages.length; i++) {
+                let img = SongImages[i]
+                await fetch(`/api/songs/${song.id}/images`, {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify(img)
+                })
+            }
+            dispatch(editSongAction(song))
+            return song;
+        }
+    } catch (e) {
+        const data = await e.json()
+        return data;
+    }
+}
+
+
+const initialState = { allSongs: {}, singleSong: {} };
+
+const songReducer = (state = initialState, action) => {
+    let newState;
+    switch (action.type) {
+        case GET_SONGS: {
+            newState = { ...state, allSongs: {}, singleSong: {} }
+            action.songs.forEach(song => newState.allSongs[song.id] = song)
+            return newState
+        }
+        case GET_SONG: {
+            newState = { ...state, allSongs: { ...state.allSongs }, singleSong: {} }
+            newState.singleSong = action.song;
+            return newState;
+        }
+        case CREATE_SONG: {
+            newState = { ...state, allSongs: { ...state.allSongs }, singleSong: {} }
+            // newState.allSongs[action.song.id] = action.song;
+            newState.singleSong = action.song;
+            return newState
+        }
+        case GET_USER_SONGS: {
+            newState = { ...state, allSongs: {}, singleSong: {} }
+            action.songs.forEach(song => newState.allSongs[song.id] = song)
+            return newState
+        }
+        case DELETE_SONG: {
+            newState = { ...state, allSongs: { ...state.allSongs }, singleSong: {} }
+            delete newState.allSongs[action.songId]
+            return newState
+        }
+        case EDIT_SONG: {
+            newState = { ...state, allSongs: { ...state.allSongs }, singleSong: {} }
+            newState.allSongs[action.song.id] = action.song;
+            return newState;
+        }
+        default:
+            return state;
+    }
+};
+
+export default songReducer;
