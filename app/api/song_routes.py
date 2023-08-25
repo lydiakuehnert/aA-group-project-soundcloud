9db199ab1da79518fdc5ef9bc10a5887045179b1
David <david_gu@yahoo.com>
2023-08-25 16:20:04 -0700

    changes to song_form and song_routes that can be implemented later

--- app/api/song_routes.py
+++ app/api/song_routes.py
@@ -1,6 +1,28 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, render_template
 from flask_login import login_required
 from app.models import User, Comment, Song
-
+from .AWS_helpers import upload_file_to_s3, get_unique_filename, remove_file_from_s3
+from ..forms.song_form import SongForm
 
 songs = Blueprint('songs', __name__)
+
+# @songs.route("/new", methods=["GET","POST"])
+# def create_new_song():
+#     form=SongForm
+
+#     # code for grabbing user
+
+#     if form.validate_on_submit():
+#         image = form.data["image"]
+#         image.filename = get_unique_filename(image.filename)
+#         upload = upload_file_to_s3(image)
+#         print(upload)
+
+#         if "url" not in upload:
+#             return render_template("<template_file_name.html>", form=form, errors=[upload])
+
+#     new_song = Song(
+#         name = form.data['name'],
+#         image = upload['url'],
+#         audio = form.data['audio']
+#     )
