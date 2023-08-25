c2c13accec9ab32907071f17243f77877dbc030d
Lydia Kuehnert <lydiakuehnert@gmail.com>
2023-08-25 11:50:41 -0400

    added some models

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,5 @@
 from .db import db
 from .user import User
 from .db import environment, SCHEMA
+from .song import Song
+from .comment import Comment
--- models(lydia)/__init__.py
+++
@@ -1,5 +0,0 @@
-from .user import User
-from .song import Song
-from .comment import Comment
-
-from .db import db
