6cb21559a252cf34736970e53207499dbc253076
Helen Coates <hcoates@Helens-MacBook-Pro.local>
2023-08-26 07:22:27 -0500

    removed body margin

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React from "react";
 import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import OpenModalButton from "../OpenModalButton";
+import LoginFormModal from "../LoginFormModal";
+import SignupFormModal from "../SignupFormModal";
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
@@ -13,8 +16,30 @@ function Navigation({ isLoaded }){
 				<NavLink id="logo" exact to="/">LoudCloud</NavLink>
 			</li>
 			
+			{sessionUser ? (
+				<></>
+
+			) : (
+				<>
+					<li className="login-button">
+					<OpenModalButton
+						buttonText="Sign In"
+						modalComponent={<LoginFormModal />}
+					/>
+					</li>
+					<li>
+					<OpenModalButton
+						buttonText="Create Account"
+						modalComponent={<SignupFormModal />}
+					/>
+					</li>
+				</>
+				)
+			}
+
+
 			{isLoaded && (
-				<li>
+				<li className="profile-button">
 					<ProfileButton user={sessionUser} />
 				</li>
 			)}
