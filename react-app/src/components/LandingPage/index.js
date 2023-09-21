6326d5401e10b0407141bfe0cb864b269318e028
Helen Coates <hcoates@Helens-MacBook-Pro.local>
2023-09-21 18:37:39 -0500

    splash text

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from 'react-router-dom';
 import SongSearch from "../SongSearch";
 import OpenModalButton from "../OpenModalButton";
 import LoginFormModal from "../LoginFormModal";
 import SignupFormModal from "../SignupFormModal";
-import { useSelector } from 'react-redux';
 import './LandingPage.css'
 import { useHistory } from 'react-router-dom';
+import { useEffect } from "react";
+import { getSongsThunk } from "../../store/songs";
+import SongCard from "../SongCard";
 
 
 
@@ -18,16 +21,48 @@ import { useHistory } from 'react-router-dom';
 export default function LandingPage() {
 	const sessionUser = useSelector(state => state.session.user);
 	const history = useHistory();
-	if (sessionUser) history.push('/home')
+	
+	const dispatch = useDispatch();
+	
+    const songsObj = useSelector(state => state.songs.allSongs);
+    const songs = Object.values(songsObj)
 
+	const songArr = []
+	
+    useEffect(() => {
+		dispatch(getSongsThunk())
+    }, [dispatch])
+	
+	if (sessionUser) history.push('/home')
+	if (!songs.length) return null
+	for (let i = 0; i < 12; i++) {
+		songArr.push(songs[i])
+	}
 
     return (
         <div className="index-landing">
             <div className="splash-image">
-            </div>
+
+			<div className="splash-text">
+					<h1 className="">What's next in music is first on LoudCloud</h1> 
+					<p className="splash-p">Upload your first track and begin your journey.</p>
+					
+					<p className="splash-p">LoudCloud gives you space to create, find your fans, and connect with other artists.</p>
+						 
+			</div>
+			</div>
 
 			<div>
-				<p>What's trending on LoudCloud</p>
+
+				<div className="hear-div">
+					<p className="hear">Hear what's trending for free in the LoudCloud community</p>
+					
+				</div>
+				<div className="all-songs">
+                {songArr.length && songArr.map(song => (
+                    <SongCard key={song.id} song={song} />
+                ))}
+            </div>
 			</div>
         
         </div>
